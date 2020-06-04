1f01c4533b8b7ee8b873728d4aa56ea3d07d3150
Ali <aifares@buffalo.edu>
2020-06-03 21:31:49 -0400

    admin

--- Raul/models.py
+++ Raul/models.py
@@ -6,9 +6,9 @@ class product(models.Model):
     title = models.CharField(max_length=128)
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(decimal_places=2, max_digits=100)
-    images = models.FileField(upload_to='raul_project/Raul/static.Raul/Images', null= True)
+    images = models.FileField(upload_to='raul_project', null=True)
 
-    slug = models.SlugField(null = True)
+    slug = models.SlugField(null=True)
     time_stamp = models.DateTimeField(auto_now_add=True, auto_now= False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
     active = models.BooleanField(default=True)
