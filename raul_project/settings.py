1997731eb1a6ced7351e3c600b30a2e962ca8af5
jonathaneugene <54916842+jonathaneugene@users.noreply.github.com>
2020-06-04 00:03:05 -0400

    Started Media For loop

--- raul_project/settings.py
+++ raul_project/settings.py
@@ -138,14 +138,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'raulwebsite069@gmail.com'
 EMAIL_HOST_PASSWORD = 'afares123'
 
-# STATIC_URL = '/static/'
-#
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static_my_proj"),
-# ]
-#
-# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
-#
-#
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
+
