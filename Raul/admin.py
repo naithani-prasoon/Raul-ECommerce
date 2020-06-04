9a4edeee53919b003ae8415e2f38c8edb05123de
Ali <aifares@buffalo.edu>
2020-06-03 20:25:07 -0400

    admin

--- Raul/admin.py
+++ Raul/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import product
 
 # Register your models here.
+
+class productAdmin(admin.ModelAdmin):
+        search_fields = ['title','description']
+        list_display = ['title', 'price', 'active', 'updated', 'description']
+        list_editable = ['price','active']
+        list_filter =['active', 'price']
+        readonly_fields = ['updated', 'time_stamp']
+        prepopulated_fields = {"slug": ("title",)}
+        class meta:
+            model = product
+admin.site.register(product,productAdmin)
+
--- Raul/models.py
+++ Raul/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class product(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=100)
+    images = models.FileField(upload_to='raul_project/Raul/static.Raul/Images', null= True)
+
+    slug = models.SlugField(null = True)
+    time_stamp = models.DateTimeField(auto_now_add=True, auto_now= False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
