fbdf8c2019fc98222fd452c0896c84b44d1a3c42
Ali <aifares@buffalo.edu>
2020-06-05 19:44:54 -0400

    added categories

--- Raul/urls.py
+++ Raul/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='Raul-Home'),
+    path('', views.landingpage, name='Raul-landing'),
+    path('raul-home/', views.home, name='Raul-Home'),
     path('home/', views.secondHome, name='Raul-SecondHome'),
-    path('product/', views.products, name='Raul-product'),
-
 
 ]
--- raul_project/urls.py
+++ raul_project/urls.py
@@ -19,12 +19,15 @@ from django.contrib.auth import views as auth_views
 from django.urls import path,include
 from users import views as user_views
 from raul_project import settings
+from Raul import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('category/<str:cats>/', views.CategoryView,name='cat-view'),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
+    path('product/', views.products, name='Raul-product'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('password-reset/',auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
