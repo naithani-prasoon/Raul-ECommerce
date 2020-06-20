474749983aa5841ca9a767882657f1f7033afbb0
DSC_UB <ub.developerstudentclub@gmail.com>
2020-06-20 04:17:46 -0400

    changes in home.html

--- Raul/static/Raul/JS/script.js
+++ Raul/static/Raul/JS/script.js
@@ -18,14 +18,21 @@
 
  
  function navbarFunction() {
-    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-        document.getElementById("hideOnScroll").style.display = "none";
-        document.getElementById("headerOnScroll").style.height = "4.5em";
-        document.getElementById("headerOnScroll").style.borderBottom = "0.5px solid white";
-    } else {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("hideOnScroll").style.color = "white";
+        document.getElementById("categoryP").style.opacity = "1";
         document.getElementById("headerOnScroll").style.height = "7.5em";
-        document.getElementById("hideOnScroll").style.display = "flex";
-        document.getElementById("headerOnScroll").style.borderBottom = "0px";
+        document.getElementById("headerOnScroll").style.background = "#333333";
+        document.getElementById("headerOnScroll").style.boxShadow = "0px 0px 5px 1px black";
+        
+    } else {
+        document.getElementById("hideOnScroll").style.background = "transparent";
+        document.getElementById("categoryP").style.opacity = "0";
+        document.getElementById("hideOnScroll").style.color = "transparent";
+        document.getElementById("headerOnScroll").style.background = "transparent";
+        document.getElementById("headerOnScroll").style.height = "7.5em";
+        document.getElementById("headerOnScroll").style.boxShadow = "0px 0px 0px 0px";
+        
         
     }
 }
