e7f967608a2a28601a62c07e1b91bac8899957aa
jonathaneugene <54916842+jonathaneugene@users.noreply.github.com>
2020-06-23 18:30:40 -0400

    Second one started

--- Raul_Inc/views.py
+++ Raul_Inc/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from Raul_Inc import templates
 
 # Create your views here.
+
 def home(request):
-    return render(request,'Raul_Inc/home.html')
+    context= {}
+    return render(request, 'Raul_Inc/home.html',context)
+
