a9b3e2732a3030975c97bd99fac1dfa9fc43f339
DSC_UB <ub.developerstudentclub@gmail.com>
2020-06-29 17:49:02 -0400

    changes

--- raul_project/urls.py
+++ raul_project/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path('profile/orders', user_views.profile, name='profile-orders'),
     path('product/', views.products, name='Raul-product'),
     path('product/<str:slug>/', views.singleView, name='Raul-single'),
-    path('login/', auth_views.LoginView.as_view(template_name='Raul/Base.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('password-reset/',auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
     path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),name='password_reset_done'),
