51a631cb43f311cc0707aaca89ca561e5fe21fee
Ali <aifares@buffalo.edu>
2020-08-13 13:46:44 -0400

    fixxes

--- orders/admin.py
+++ orders/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Order, StripeInfo
+class orderAdmin(admin.ModelAdmin):
+    search_fields = ['user','Tracking_Status']
+    list_display = ['user', 'final_total', 'order_pdf']
+    list_filter =['user', 'Tracking_Status']
+    readonly_fields = ['updated']
+    class meta:
+        model = Order
 
-admin.site.register(Order)
+admin.site.register(Order,orderAdmin)
 admin.site.register(StripeInfo)
